fb7c02caa062860d483f1e227dc3338c409fb5d5
SribathKarnan <sribathmarley@gmail.com>
2023-08-10 19:21:32 +0530

    sribath

---
+++ React-FrontEnd/Day-2/my-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1>Guvi-B49-React day 2</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ React-FrontEnd/Day-3/vite-project/src/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>My first componnent</div>
+  )
+}
+
+export default Home
