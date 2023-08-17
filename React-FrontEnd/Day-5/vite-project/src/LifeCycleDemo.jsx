ae1d7b053c6810d47623a4234bf747c207a9e00f
SribathKarnan <sribathmarley@gmail.com>
2023-08-17 19:48:55 +0530

    sribath

--- React-FrontEnd/Day-5/vite-project/src/LifeCycleDemo.jsx
+++ React-FrontEnd/Day-5/vite-project/src/LifeCycleDemo.jsx
@@ -1,18 +1,29 @@
-import React, {useStates,useEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function LifeCycleDemo() {
-  //Mounting phase
-  const [count, setCount] = useState(0);
+    //Mounting Phase
+    const [count, setCount] = useState(0);
 
-    useEffect(()=>
-    console.log("Component Mounted/Loaded!! "));
-    //return()=>{
-      //console.log("Component unMounted!!");
-      //}
-},[]);//Empty dependency array runs this effect only once after the initial render
+    useEffect(()=>{
+        console.log("Component Mounted/Loaded!!");
+        // return () => {
+        //     console.log("Component unmounted!!!");
+        // }
+    },[]); //Empty dependency array runs this effect only once after the initial render
 
+    //Updating Phase
+    useEffect(() => {
+        console.log('Component updated!!!');
+        // return () => {
+        //     console.log("Component updated cleanup!!");
+        // };
+    }); //No dependency array, runs after every render
   return (
-    <div>LifeCycleDemo</div>
+    <div>
+        <h1>LifeCycleDemo</h1>
+        <p>Count: {count} </p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
   )
 }
 
